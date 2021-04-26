294b67f0a45f4f78ca5d576a69fb47b04447829f
Andresuribe9420 <sarkodos9420@gmail.com>
2021-04-26 17:11:22 -0500

    BaseDatos

--- config/config.php
+++ config/config.php
@@ -1,6 +1,20 @@
+<!-- <?php 
+    // define('SERVIDOR','localhost');
+    // define('DATABASE','zoologicooktal');
+    // define('PASSWORD','');
+    // define('USER','root');
+?> -->
 <?php 
-    define('SERVIDOR','localhost');
-    define('DATABASE','zoologicooktal');
-    define('PASSWORD','');
-    define('USER','root');
+    define('SERVIDOR','us-cdbr-east-03.cleardb.com');
+    define('DATABASE','heroku_ed79454636ea7ae');
+    define('PASSWORD','3c57cb2d');
+    define('USER','b2398b99625e83');
 ?>
+
+
+<!-- 
+b2398b99625e83
+
+3c57cb2d
+
+us-cdbr-east-03.cleardb.com -->
